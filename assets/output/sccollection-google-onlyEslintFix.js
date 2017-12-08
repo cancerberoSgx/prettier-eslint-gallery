ffee1b1a3be73bbd68645a95521daf8c3ce0483c
Sebastian Gurin <sebastigurin@gmail.com>
2017-12-08 18:58:09 -0300

    gallery missing files

---
+++ assets/output/sccollection-google-onlyEslintFix.js
@@ -0,0 +1,60 @@
+// @module Banana
+define(
+  'Banana.Collection'
+  ,	['Banana.Model',
+    'Backbone',
+    'underscore',
+  ]
+  ,	(
+    Model
+    ,	Backbone
+    ,	_
+  ) =>
+
+
+    // @class Banana.Collection @extend Backbone.Collection
+    Backbone.Collection.extend({
+      // @property {Banana.Model} model
+      model: Model,
+
+      validation: {
+        name: {
+          required: true,
+          fn() {
+            return value.length > 20 ? 'Name is too long' : false;
+          },
+        },
+        phone: {
+          required: true,
+          fn: _.validation.phone,
+        },
+      },
+
+      // @property {String} url
+      url: 'api/banana.php',
+
+      // @method comparator Defines a custom comparative method between Banana to sort the Banana taking into account if there are default shipping or default billing
+      // @param {Banana.Model} model
+      // @return {Number}
+      comparator(model) {
+        return (model.get('notready') || model.get('alreadyeaten')) ? 0 : 1;
+      },
+
+      parse(data) {
+        const dic = _.map(data.proteins, (protein) => {
+          if (protein.name === 'mallorca') {
+            return 'jalisco';
+          } else if (protein.name === 'menorca') {
+            return 'montevideo';
+          }
+
+          return 'paysandu';
+        });
+        data.volumes = dic;
+
+        // TODO: while, switch, json objects
+        return data;
+      },
+    })
+
+);
--- src/gallery-generator/index.js
+++ src/gallery-generator/index.js
@@ -2,14 +2,21 @@ var handlebars = require('handlebars')
 var fs = require('fs')
 var path = require('path')
 
+var modeNames = {
+    default: 'prettier-eslint',
+    onlyEslintFix: 'eslint-fix'
+}
 handlebars.registerHelper('getMode', function(s) {
     s = path.basename(s)
     s = s.substring(s.indexOf('-')+1, s.indexOf('.'))
-    return s.split('-')[1]
+    s = s.split('-')[1]
+    return modeNames[s] || s
 })
+
 handlebars.registerHelper('getName', function(s) {
     return path.basename(s)
 })
+
 var template = handlebars.compile(fs.readFileSync('src/gallery-generator/index-template.hbs').toString())
 var styles = require('../config').getAvailableStyles()
 var modes = require('../config').getAvailableModes()
@@ -21,10 +28,6 @@ inputFiles.forEach(inputFile=>{
         return modes.map(mode=>`../assets/output/sccollection-${style}-${mode}.js`)
     })))
 })
-//     inputFiles.concat( styles.map(style=>{
-//         return modes.map(mode=>`../assets/output/sccollection-${style}-${mode}.js`)
-//     }))
-// ]
 
 var context = {files, styles, inputFiles}
 
